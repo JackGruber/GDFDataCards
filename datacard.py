6952c902631d04fd32d326cb51545a05670ddbc5
JackGruber <24863925+JackGruber@users.noreply.github.com>
2023-04-30 09:04:41 +0200

    Create all unit datacards

--- datacard.py
+++ datacard.py
@@ -21,9 +21,7 @@ def Main():
     createDataCard(army['units'][3])
 
 
-def createDataCard(unit):
-    fileName = 'sample.pdf'
-    documentTitle = 'sample'
+def createDataCard(units):
 
     pdfmetrics.registerFont(TTFont('bold', os.path.join(
         FONTFOLDER, "rosa-sans", "hinted-RosaSans-Bold.ttf")))
@@ -33,133 +31,139 @@ def createDataCard(unit):
     datacardSize = (200.0, 130.0)
     lineColor = [1.00, 0.55, 0.10]
     # creating a pdf object
-    pdf = canvas.Canvas(fileName, pagesize=datacardSize)
-    pdf.setTitle(documentTitle)
+    pdf = canvas.Canvas("sample.pdf", pagesize=datacardSize)
+    pdf.setTitle("GDF data card")
 
-    # Card Box
-    topClearance = 10
-    bottomClearance = 10
-    sideClearance = 2
-    pdf.setStrokeColorRGB(lineColor[0], lineColor[1], lineColor[2])
-    path = pdf.beginPath()
-    path.moveTo(0 + sideClearance, 0 + bottomClearance)
-    path.lineTo(0 + sideClearance, datacardSize[1] - topClearance)
-    path.lineTo(datacardSize[0] - sideClearance,
-                datacardSize[1] - topClearance)
-    path.lineTo(datacardSize[0] - sideClearance,
-                0 + bottomClearance)
-    path.close()
-    pdf.setLineJoin(1)
-    pdf.drawPath(path, stroke=1, fill=0)
-    pdf.line(sideClearance, datacardSize[1] - 45,
-             datacardSize[0]-sideClearance, datacardSize[1] - 45)
+    for unit in units:
+        # Card Box
+        topClearance = 10
+        bottomClearance = 10
+        sideClearance = 2
+        pdf.setStrokeColorRGB(lineColor[0], lineColor[1], lineColor[2])
+        path = pdf.beginPath()
+        path.moveTo(0 + sideClearance, 0 + bottomClearance)
+        path.lineTo(0 + sideClearance, datacardSize[1] - topClearance)
+        path.lineTo(datacardSize[0] - sideClearance,
+                    datacardSize[1] - topClearance)
+        path.lineTo(datacardSize[0] - sideClearance,
+                    0 + bottomClearance)
+        path.close()
+        pdf.setLineJoin(1)
+        pdf.drawPath(path, stroke=1, fill=0)
+        pdf.line(sideClearance, datacardSize[1] - 45,
+                 datacardSize[0]-sideClearance, datacardSize[1] - 45)
 
-    # Bottom Info Box
-    pdf.setStrokeColorRGB(lineColor[0], lineColor[1], lineColor[2])
-    pdf.setFillColorRGB(1, 1, 1)
-    path = pdf.beginPath()
-    sideClearance = 20
-    sideClearance = 20
-    height = 10
-    bottomClearance = 5
-    path.moveTo(0 + sideClearance, 0 + bottomClearance)
-    path.lineTo(datacardSize[0] - sideClearance, 0 + bottomClearance)
-    path.lineTo(datacardSize[0] - sideClearance, 0 + bottomClearance + height)
-    path.lineTo(0 + sideClearance, 0 + bottomClearance + height)
-    path.close()
-    pdf.setLineJoin(1)
-    pdf.drawPath(path, stroke=1, fill=1)
-    pdf.setFont('bold', 4)
-    pdf.setFillColorRGB(0, 0, 0)
-    pdf.drawString(sideClearance+2, bottomClearance +
-                   (height/2)-1, ", ".join(unit['specialRules']))
-
-    # Image box
-    with Image.open('spacemarine.png') as img:
-        img.load()
-        imgSize = img.size
-        draw = ImageDraw.Draw(img)
-        draw.polygon(((0, 0), (imgSize[0]/2, imgSize[1]),
-                     (0, imgSize[1])), fill=(0, 255, 0))
-        draw.polygon(((imgSize[0], 0), (imgSize[0]/2, imgSize[1]),
-                     (imgSize[0], imgSize[1])), fill=(0, 255, 0))
-        img.save(os.path.join(DATAFOLDER, "img.png"))
-
-    edgeLength = 60
-    offsetTop = 2
-    offsetRight = 35
-    triangle = [
-        [datacardSize[0] - edgeLength - offsetRight, datacardSize[1] - offsetTop],
-        [datacardSize[0] - offsetRight, datacardSize[1] - offsetTop],
-        [datacardSize[0] - (edgeLength/2) - offsetRight,
-         datacardSize[1] - offsetTop - edgeLength]
-    ]
-
-    pdf.setStrokeColorRGB(lineColor[0], lineColor[1], lineColor[2])
-    pdf.drawImage(os.path.join(DATAFOLDER, "img.png"), datacardSize[0] - offsetRight - edgeLength,
-                  datacardSize[1] - offsetTop - edgeLength, edgeLength, edgeLength, mask=[0, 0, 255, 255, 0, 0])
-    path = pdf.beginPath()
-    path.moveTo(triangle[0][0], triangle[0][1])
-    path.lineTo(triangle[1][0], triangle[1][1])
-    path.lineTo(triangle[2][0], triangle[2][1])
-    path.close()
-    pdf.setLineJoin(1)
-    pdf.drawPath(path, stroke=1, fill=0)
-
-    # Unit Name
-    pdf.setFont('bold', 10)
-    pdf.setFillColorRGB(0, 0, 0)
-    pdf.drawString(5, datacardSize[1] - 25, unit['name'])
-
-    # Skills
-    startX = datacardSize[0] - 25
-    startY = datacardSize[1] - 20
-    lineHight = 5
-    pdf.setFont('bold', lineHight)
-    pdf.setFillColorRGB(0, 0, 0)
-    pdf.drawCentredString(startX, startY, "Quality")
-    pdf.drawCentredString(startX, startY - (lineHight*3), "Defense")
-    pdf.setFont('regular', 5)
-    pdf.drawCentredString(startX, startY - (lineHight*1), unit["quality"])
-    pdf.drawCentredString(startX, startY - (lineHight*4), unit["defense"])
-
-    # Weapon
-    startX = 5
-    startY = datacardSize[1] - 70
-    offsetY = 0
-    headers = {'name': 'Weapon', 'range': 'Range', 'attacks': 'Attacks',
-               'ap': 'AP', 'specialRules': ['Special rules']}
-    unit['weapon'].insert(0, headers)
-    font = "bold"
-    for weapon in unit['weapon']:
-        offsetX = 0
-        pdf.setFont(font, 5)
+        # Bottom Info Box
+        pdf.setStrokeColorRGB(lineColor[0], lineColor[1], lineColor[2])
+        pdf.setFillColorRGB(1, 1, 1)
+        path = pdf.beginPath()
+        sideClearance = 20
+        sideClearance = 20
+        height = 10
+        bottomClearance = 5
+        path.moveTo(0 + sideClearance, 0 + bottomClearance)
+        path.lineTo(datacardSize[0] - sideClearance, 0 + bottomClearance)
+        path.lineTo(datacardSize[0] - sideClearance,
+                    0 + bottomClearance + height)
+        path.lineTo(0 + sideClearance, 0 + bottomClearance + height)
+        path.close()
+        pdf.setLineJoin(1)
+        pdf.drawPath(path, stroke=1, fill=1)
+        pdf.setFont('bold', 4)
         pdf.setFillColorRGB(0, 0, 0)
+        pdf.drawString(sideClearance+2, bottomClearance +
+                       (height/2)-1, ", ".join(unit['specialRules']))
 
-        pdf.drawString(startX, startY + offsetY, weapon['name'])
-        offsetX += 70
-        if "range" in weapon:
-            pdf.drawString(startX + offsetX, startY + offsetY, weapon['range'])
-        else:
-            pdf.drawString(startX + offsetX, startY + offsetY, "-")
-        offsetX += 20
-        pdf.drawString(startX + offsetX, startY + offsetY, weapon['attacks'])
-        offsetX += 25
-        if "ap" in weapon:
-            pdf.drawString(startX + offsetX, startY + offsetY, weapon['ap'])
-        else:
-            pdf.drawString(startX + offsetX, startY + offsetY, "-")
-        offsetX += 15
-        if "specialRules" in weapon:
-            pdf.drawString(startX + offsetX, startY + offsetY,
-                           ", ".join(weapon['specialRules']))
-        else:
-            pdf.drawString(startX + offsetX, startY + offsetY, "-")
-        offsetX += 20
+        # Image box
+        with Image.open('spacemarine.png') as img:
+            img.load()
+            imgSize = img.size
+            draw = ImageDraw.Draw(img)
+            draw.polygon(((0, 0), (imgSize[0]/2, imgSize[1]),
+                          (0, imgSize[1])), fill=(0, 255, 0))
+            draw.polygon(((imgSize[0], 0), (imgSize[0]/2, imgSize[1]),
+                          (imgSize[0], imgSize[1])), fill=(0, 255, 0))
+            img.save(os.path.join(DATAFOLDER, "img.png"))
 
-        offsetY -= 8
-        font = "regular"
+        edgeLength = 60
+        offsetTop = 2
+        offsetRight = 35
+        triangle = [
+            [datacardSize[0] - edgeLength - offsetRight,
+                datacardSize[1] - offsetTop],
+            [datacardSize[0] - offsetRight, datacardSize[1] - offsetTop],
+            [datacardSize[0] - (edgeLength/2) - offsetRight,
+             datacardSize[1] - offsetTop - edgeLength]
+        ]
 
+        pdf.setStrokeColorRGB(lineColor[0], lineColor[1], lineColor[2])
+        pdf.drawImage(os.path.join(DATAFOLDER, "img.png"), datacardSize[0] - offsetRight - edgeLength,
+                      datacardSize[1] - offsetTop - edgeLength, edgeLength, edgeLength, mask=[0, 0, 255, 255, 0, 0])
+        path = pdf.beginPath()
+        path.moveTo(triangle[0][0], triangle[0][1])
+        path.lineTo(triangle[1][0], triangle[1][1])
+        path.lineTo(triangle[2][0], triangle[2][1])
+        path.close()
+        pdf.setLineJoin(1)
+        pdf.drawPath(path, stroke=1, fill=0)
+
+        # Unit Name
+        pdf.setFont('bold', 10)
+        pdf.setFillColorRGB(0, 0, 0)
+        pdf.drawString(5, datacardSize[1] - 25, unit['name'])
+
+        # Skills
+        startX = datacardSize[0] - 25
+        startY = datacardSize[1] - 20
+        lineHight = 5
+        pdf.setFont('bold', lineHight)
+        pdf.setFillColorRGB(0, 0, 0)
+        pdf.drawCentredString(startX, startY, "Quality")
+        pdf.drawCentredString(startX, startY - (lineHight*3), "Defense")
+        pdf.setFont('regular', 5)
+        pdf.drawCentredString(startX, startY - (lineHight*1), unit["quality"])
+        pdf.drawCentredString(startX, startY - (lineHight*4), unit["defense"])
+
+        # Weapon
+        startX = 5
+        startY = datacardSize[1] - 70
+        offsetY = 0
+        headers = {'name': 'Weapon', 'range': 'Range', 'attacks': 'Attacks',
+                   'ap': 'AP', 'specialRules': ['Special rules']}
+        unit['weapon'].insert(0, headers)
+        font = "bold"
+        for weapon in unit['weapon']:
+            offsetX = 0
+            pdf.setFont(font, 5)
+            pdf.setFillColorRGB(0, 0, 0)
+
+            pdf.drawString(startX, startY + offsetY, weapon['name'])
+            offsetX += 70
+            if "range" in weapon:
+                pdf.drawString(startX + offsetX, startY +
+                               offsetY, weapon['range'])
+            else:
+                pdf.drawString(startX + offsetX, startY + offsetY, "-")
+            offsetX += 20
+            pdf.drawString(startX + offsetX, startY +
+                           offsetY, weapon['attacks'])
+            offsetX += 25
+            if "ap" in weapon:
+                pdf.drawString(startX + offsetX, startY +
+                               offsetY, weapon['ap'])
+            else:
+                pdf.drawString(startX + offsetX, startY + offsetY, "-")
+            offsetX += 15
+            if "specialRules" in weapon:
+                pdf.drawString(startX + offsetX, startY + offsetY,
+                               ", ".join(weapon['specialRules']))
+            else:
+                pdf.drawString(startX + offsetX, startY + offsetY, "-")
+            offsetX += 20
+
+            offsetY -= 8
+            font = "regular"
+        pdf.showPage()
     pdf.save()
 
 
