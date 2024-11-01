49d273f1e27de5b6d082fcc975b920b144b010d3
JackGruber <24863925+JackGruber@users.noreply.github.com>
2024-11-01 12:20:58 +0100

    Fix_ missing parameter

--- testing/test_download.py
+++ testing/test_download.py
@@ -9,13 +9,13 @@ import OPRDatacard  # nopep8
 
 def test_Ok():
     OPRDatacard.DATAFOLDERARMYBOOK = os.path.join(testhelper.TESTDIR, 'tmp', 'data')
-    OPRDatacard.set_settings(False, True)
+    OPRDatacard.set_settings(False, True, False)
     result = OPRDatacard.downloadArmyBook("7oi8zeiqfamiur21", 3)
     assert result == True
 
 
 def test_Error():
     OPRDatacard.DATAFOLDERARMYBOOK = os.path.join(testhelper.TESTDIR, 'tmp', 'data')
-    OPRDatacard.set_settings(False, True)
+    OPRDatacard.set_settings(False, True, False)
     result = OPRDatacard.downloadArmyBook("no", 3)
     assert result == False
